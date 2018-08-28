c332a14e870f06bac868c99314439340136ddf15
SebPud <sebapudelko93@gmail.com>
2018-08-28 14:48:31 +0200

    Kasparow Deep Blue

---
+++ Course1/Chess/src/Chess/Checkers.java
@@ -0,0 +1,16 @@
+package Chess;
+
+public enum Checkers {
+    B_KING,
+    B_QUEEN,
+    B_ROOK,
+    B_BISHOP,
+    B_KNIGHT,
+    B_PAWN,
+    W_KING,
+    W_QUEEN,
+    W_ROOK,
+    W_BISHOP,
+    W_KNIGHT,
+    W_PAWN,
+}
